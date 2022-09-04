5fd98958baf9ad236d0a16e00a0fd2d421a28b7b
Gleb Trunov <gtrunov89@gmail.com>
2022-09-04 15:13:17 +0300

    3rd

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        int price = 550;
-        int Miles = price / 20;
-        String result = "Bonus Miles: ";
-
-        System.out.println(result + Miles);
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной милли
-
-        // Рассчитываете количество бонусных милль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
+        BonusMilesService service = new BonusMilesService();
+        int bonusMiles = service.calculate(20000);
+        System.out.println("20000");
+        System.out.println(bonusMiles);
+        System.out.println();
+        System.out.println("30000");
+        System.out.println(service.calculate(30000));
+        System.out.println();
+        System.out.println("40000");
+        System.out.println(service.calculate(40000));
+        System.out.println();
+        System.out.println("50000");
+        System.out.println(service.calculate(50000));
+        System.out.println();
     }
 }
